e656c376957ebae85417ecbeff00f8017dbec1f5
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 10:34:03 -0500

    rute /api/v1/methods

--- src/routes/_index.routes.js
+++ src/routes/_index.routes.js
@@ -4,5 +4,6 @@ const router = require('express').Router()
 
 router.use('/accounts', require('@app/routes/accounts.routes'))
 router.use('/deposits', require('@app/routes/deposits.routes'))
+router.use('/methods', require('@app/routes/methods.routes'))
 
 module.exports = router
