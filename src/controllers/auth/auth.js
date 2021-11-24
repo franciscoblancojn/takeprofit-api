1f4ef6d2a25b24a471852172de3a14095f3aa9e5
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 16:26:28 -0500

    [POST] /auth

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -1,6 +1,8 @@
 require("module-alias/register");
-const {} = require("@app/functions/_index");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+const env = dotenv.parsed;
+const { comparePassword } = require("@app/functions/_index");
 const db = require("@app/db");
 
 const index = async (req, res) => {
@@ -8,7 +10,7 @@ const index = async (req, res) => {
         const body = req.body;
         const result = await db.get({
             table: "accounts",
-            query:{
+            query: {
                 email: body.email,
             },
         });
@@ -16,9 +18,19 @@ const index = async (req, res) => {
             throw new Error("Email o Password invalid");
         }
         const user = result[0];
+        const verifitPassword = await comparePassword(
+            body.password,
+            user.password
+        );
+        if (!verifitPassword) {
+            throw new Error("Email o Password invalid");
+        }
+        delete user.password
+        const token = jwt.sign(user, env.JWT)
+
         return res.send({
             type: "ok",
-            respond: {},
+            respond: {token},
         });
     } catch (error) {
         return res.status(error.code || 500).send({
