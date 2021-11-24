516cb6918a9f73dd62e5314f53a7431a88b5f8ca
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 10:17:22 -0500

    CRUD acount

--- src/controllers/acounts/_index.js
+++ src/controllers/acounts/_index.js
@@ -1,3 +1,6 @@
 module.exports = {
-    
+    get : require("@app/controllers/acounts/get"),
+    post : require("@app/controllers/acounts/post"),
+    put : require("@app/controllers/acounts/put"),
+    delete : require("@app/controllers/acounts/delete"),
 }
