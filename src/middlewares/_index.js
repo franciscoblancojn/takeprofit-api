3124f2c48599ff11bb318bbace9ee12a8eccf10f
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 16:29:32 -0500

    middlewares auth

--- src/middlewares/_index.js
+++ src/middlewares/_index.js
@@ -2,4 +2,5 @@ module.exports = {
     apikey : require("@app/middlewares/apikey"),
     jwt : require("@app/middlewares/jwt"),
     accounts : require("@app/middlewares/accounts/_index"),
+    auth : require("@app/middlewares/auth/_index"),
 }
---
+++ src/middlewares/auth/_index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    auth : require("@app/middlewares/auth/auth"),
+}
