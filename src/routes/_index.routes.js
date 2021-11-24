4e5708c25b4e2f6edce678fd8fe9690ba01a70c2
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 10:44:15 -0500

    rute /api/v1/auth

--- src/routes/_index.routes.js
+++ src/routes/_index.routes.js
@@ -2,6 +2,7 @@ require('module-alias/register')
 
 const router = require('express').Router()
 
+router.use('/auth', require('@app/routes/auth.routes'))
 router.use('/accounts', require('@app/routes/accounts.routes'))
 router.use('/deposits', require('@app/routes/deposits.routes'))
 router.use('/methods', require('@app/routes/methods.routes'))
