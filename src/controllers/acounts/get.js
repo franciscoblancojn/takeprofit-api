6ad12228c429a4c908808ec53202cf636a919a41
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 10:12:51 -0500

    editorconfig

---
+++ src/controllers/acounts/get.js
@@ -0,0 +1,18 @@
+require("module-alias/register");
+const db = require("@app/db");
+
+const index = async (req, res) => {
+    try {
+        return res.send({
+            type: "ok",
+            respond: {},
+        });
+    } catch (error) {
+        return res.status(error.code || 500).send({
+            type: "error",
+            error,
+            msj: error.msj || `${error}`,
+        });
+    }
+};
+module.exports = index;
