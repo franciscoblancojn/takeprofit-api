de1420ef2df23f4301d682c0ddc1cd99cfadcc7b
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 10:29:06 -0500

    rute /api/v1/deposits

--- src/routes/_index.routes.js
+++ src/routes/_index.routes.js
@@ -3,5 +3,6 @@ require('module-alias/register')
 const router = require('express').Router()
 
 router.use('/accounts', require('@app/routes/accounts.routes'))
+router.use('/deposits', require('@app/routes/deposits.routes'))
 
 module.exports = router
