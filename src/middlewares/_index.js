d6ac8ab166044ca6aeab0da2493b5d587b8535e2
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 16:14:31 -0500

    use jwt

--- src/middlewares/_index.js
+++ src/middlewares/_index.js
@@ -1,4 +1,5 @@
 module.exports = {
     apikey : require("@app/middlewares/apikey"),
+    jwt : require("@app/middlewares/jwt"),
     accounts : require("@app/middlewares/accounts/_index"),
 }
--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -1,11 +1,11 @@
 require("module-alias/register");
 const router = require("express").Router();
-const { apikey, accounts } = require("@app/middlewares/_index");
+const { apikey, accounts, jwt } = require("@app/middlewares/_index");
 const controller = require("@app/controllers/accounts/_index");
 
 router.get("/", [apikey, accounts.get], controller.get);
 router.post("/", [apikey, accounts.post], controller.post);
-router.put("/", [apikey,...accounts.put], controller.put);
+router.put("/", [apikey,jwt,...accounts.put], controller.put);
 router.delete("/", [apikey], controller.delete);
 
 module.exports = router;
