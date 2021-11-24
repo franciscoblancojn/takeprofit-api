7f70ca4079bf4ec23dd7497ffedf531aae09c0b8
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-24 10:38:49 -0500

    retiros -> retreats

---
+++ src/controllers/retreats/_index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    get : require("@app/controllers/retreats/get"),
+    post : require("@app/controllers/retreats/post"),
+    put : require("@app/controllers/retreats/put"),
+    delete : require("@app/controllers/retreats/delete"),
+}
