60e96594a906a47cff713d7ac9a83c4bd93d8061
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-25 08:33:46 -0500

    [GET] /deposis

--- src/controllers/deposits/get.js
+++ src/controllers/deposits/get.js
@@ -4,9 +4,17 @@ const db = require("@app/db");
 
 const index = async (req, res) => {
     try {
+        const user_id = req.jwt__._id
+        const result = await db.get({
+            table:"deposits",
+            query:{
+                ...req.query,
+                user_id
+            },
+        })
         return res.send({
             type: "ok",
-            respond: {},
+            respond: result,
         });
     } catch (error) {
         return res.status(error.code || 500).send({
