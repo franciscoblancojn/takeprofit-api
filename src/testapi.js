e15c8c648b5e08af89504e5ffeb0269bed9a287e
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-26 12:14:19 -0500

    change testapi

--- src/testapi.js
+++ src/testapi.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 
-const port = 5050
+const port = 8080
 const cors = require('cors')
 app.use(cors({
   origin: '*'
 }))
+app.use(express.json({extended:false}))
 
 app.set('port', port)
 
@@ -13,7 +14,6 @@ app.use('*', (req, res) => {
   res.status(404).send({ status: 404, msg: "this endpoint doesn't exist", type: 'error', code: 404 })
 })
 
-const server = require('http').createServer(app)
-server.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
   console.log(`server on port: ${app.get('port')}`)
 })
