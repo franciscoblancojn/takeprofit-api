a36f8f1fb94d29432a8db937f5149c59405a6914
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-26 12:20:46 -0500

    reset

--- src/testapi.js
+++ src/testapi.js
@@ -1,3 +1,19 @@
-module.exports = async (req, res) => {
-    res.status(200).json({ test: 1 });
-};
+const express = require('express')
+const app = express()
+
+const port = 8080
+const cors = require('cors')
+app.use(cors({
+  origin: '*'
+}))
+app.use(express.json({extended:false}))
+
+app.set('port', port)
+
+app.use('*', (req, res) => {
+  res.status(404).send({ status: 404, msg: "this endpoint doesn't exist", type: 'error', code: 404 })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`server on port: ${app.get('port')}`)
+})
