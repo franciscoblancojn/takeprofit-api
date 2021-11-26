d9f0c4a5af80e90a38f6cdf75e3d1e8d3cc07553
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-26 09:35:04 -0500

    [PUT] /methods middleware

--- src/routes/methods.routes.js
+++ src/routes/methods.routes.js
@@ -5,7 +5,7 @@ const controller = require("@app/controllers/methods/_index");
 
 router.get("/", [apikey,jwt,methods.get], controller.get);
 router.post("/", [apikey,jwt,methods.post], controller.post);
-router.put("/", [apikey], controller.put);
+router.put("/", [apikey],jwt,methods.put, controller.put);
 router.delete("/", [apikey], controller.delete);
 
 module.exports = router;
