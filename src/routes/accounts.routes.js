56aec6466d5120cd350adc4c2078041c0f498ca8
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-29 18:52:30 -0500

    jwt int [GET] /accounts

--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { apikey, accounts, jwt } = require("@app/middlewares/_index");
 const controller = require("@app/controllers/accounts/_index");
 
-router.get("/", [apikey, accounts.get], controller.get);
+router.get("/", [apikey,jwt, accounts.get], controller.get);
 router.post("/", [apikey, accounts.post], controller.post);
 router.put("/", [apikey,jwt,...accounts.put], controller.put);
 router.delete("/", [apikey,jwt,accounts.delete], controller.delete);
