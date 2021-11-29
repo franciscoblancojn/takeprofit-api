0b3d795c76138ac4578ce17a32f582274045facc
franciscoblancojn <blancofrancisco34@gmail.com>
2021-11-29 11:03:47 -0500

    change verify

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,6 @@ const {apikey,auth,jwt} = require("@app/middlewares/_index");
 const controller = require("@app/controllers/auth/_index");
 
 router.post("/", [apikey,auth.auth], controller.auth);
-router.post("/verifit", [apikey,jwt], controller.auth);
+router.post("/verify", [apikey,jwt], controller.auth);
 
 module.exports = router;
