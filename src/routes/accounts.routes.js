e2432dce27ce65674823a116f4f246086734a73b
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-01 10:48:40 -0500

    role admin in [GET] accounts

--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -1,11 +1,11 @@
 require("module-alias/register");
 const router = require("express").Router();
-const { apikey, accounts, jwt } = require("@app/middlewares/_index");
+const { apikey, accounts, jwt, role } = require("@app/middlewares/_index");
 const controller = require("@app/controllers/accounts/_index");
 
-router.get("/", [apikey,jwt, accounts.get], controller.get);
+router.get("/", [apikey, jwt, role("admin"), accounts.get], controller.get);
 router.post("/", [apikey, accounts.post], controller.post);
-router.put("/", [apikey,jwt,...accounts.put], controller.put);
-router.delete("/", [apikey,jwt,accounts.delete], controller.delete);
+router.put("/", [apikey, jwt, ...accounts.put], controller.put);
+router.delete("/", [apikey, jwt, accounts.delete], controller.delete);
 
 module.exports = router;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 require("module-alias/register");
 const router = require("express").Router();
-const {apikey,auth,jwt,role} = require("@app/middlewares/_index");
+const { apikey, auth, jwt, role } = require("@app/middlewares/_index");
 const controller = require("@app/controllers/auth/_index");
 
-router.post("/", [apikey,auth.auth], controller.auth);
-router.post("/verify/admin", [apikey,jwt,role("admin")], controller.verifit);
-router.post("/verify", [apikey,jwt], controller.verifit);
-router.post("/admin", [apikey,auth.auth], controller.admin);
+router.post("/", [apikey, auth.auth], controller.auth);
+router.post("/verify/admin", [apikey, jwt, role("admin")], controller.verifit);
+router.post("/verify", [apikey, jwt], controller.verifit);
+router.post("/admin", [apikey, auth.auth], controller.admin);
 
 module.exports = router;
