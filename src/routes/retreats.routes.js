7fbc1ab1180596ae70b7c7c315dd24b409d28085
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-01 12:39:45 -0500

    /retreats/admin

--- src/routes/retreats.routes.js
+++ src/routes/retreats.routes.js
@@ -1,10 +1,15 @@
 require("module-alias/register");
 const router = require("express").Router();
-const {apikey,jwt,retreats} = require("@app/middlewares/_index");
+const { apikey, jwt, retreats, role } = require("@app/middlewares/_index");
 const controller = require("@app/controllers/retreats/_index");
 
-router.get("/", [apikey,jwt,retreats.get], controller.get);
-router.post("/", [apikey,jwt,retreats.post], controller.post);
+router.get("/", [apikey, jwt, retreats.get], controller.get);
+router.get(
+    "/admin",
+    [apikey, jwt, role("admin"), deposits.get],
+    controller.get
+);
+router.post("/", [apikey, jwt, retreats.post], controller.post);
 router.put("/", [apikey], controller.put);
 router.delete("/", [apikey], controller.delete);
 
