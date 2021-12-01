d289a27fa91eedc3da6b48bda3bf84803bb740ea
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-01 10:23:25 -0500

    controller admin

---
+++ src/controllers/auth/admin.js
@@ -0,0 +1,48 @@
+require("module-alias/register");
+const jwt = require("jsonwebtoken");
+const env = require("@app/env")
+const { comparePassword } = require("@app/functions/_index");
+const db = require("@app/db");
+
+const index = async (req, res) => {
+    try {
+        const body = req.body;
+        const result = await db.get({
+            table: "accounts",
+            query: {
+                email: body.email,
+            },
+        });
+        if(result.type==="error"){
+            throw result
+        }
+        if (result.length === 0) {
+            throw new Error("Email o Password invalid");
+        }
+        const user = result[0];
+        if (user.role !== "admin") {
+            throw new Error("Email o Password invalid");
+        }
+        const verifitPassword = await comparePassword(
+            body.password,
+            user.password
+        );
+        if (!verifitPassword) {
+            throw new Error("Email o Password invalid");
+        }
+        delete user.password
+        const token = jwt.sign(user, env.JWT,{ expiresIn: '2h' })
+
+        return res.send({
+            type: "ok",
+            respond: {token},
+        });
+    } catch (error) {
+        return res.status(error.code || 500).send({
+            type: "error",
+            error,
+            msj: error.msj || `${error}`,
+        });
+    }
+};
+module.exports = index;
