dac21f6fd9269c49b8d14f2908bbae87ca343ab1
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-14 15:11:27 -0500

    validate methods in retreats

--- src/middlewares/retreats/post.js
+++ src/middlewares/retreats/post.js
@@ -1,4 +1,5 @@
 const fmiddlewares = require("fmiddlewares");
+const db = require("@app/db");
 
 module.exports = [
     fmiddlewares.validateItem({
@@ -12,8 +13,38 @@ module.exports = [
         monto: {
             type: "number",
         },
-        account: {
+        methods: {
             type: "string",
         },
     }),
+    async (req, res, next) => {
+        try {
+            const user_id = req.jwt__._id;
+            const _id = req.body.methods;
+            const result = await db.get({
+                table: "retreats",
+                query: {
+                    _id,
+                    user_id,
+                },
+            });
+            if (!result) {
+                throw {
+                    error: "methods invalid",
+                };
+            }
+            if (result.length == 0) {
+                throw {
+                    error: "methods invalid",
+                };
+            }
+        } catch (error) {
+            return res.status(400).send({
+                type: "error",
+                error,
+                msj: `${error}`,
+            });
+        }
+        next();
+    },
 ];
