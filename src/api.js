e815f701b1f29e957e2c6f0796002b5efb7587ab
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-15 18:22:04 -0500

    whitelist in api

--- src/api.js
+++ src/api.js
@@ -1,49 +1,69 @@
-require('module-alias/register')
-const env = require("@app/env")
-const express = require('express')
-const app = express()
+require("module-alias/register");
+const env = require("@app/env");
+const express = require("express");
+const app = express();
 
-const port = env.PORT || 3001
-const path = require('path')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-app.use(cors({
-  origin: '*'
-}))
+const port = env.PORT || 3001;
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const morgan = require('morgan')
+const whitelist = env.WHITELIST.split(",,,");
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (whitelist.include(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+    })
+);
 
-const routes = require('@app/routes/_index.routes')
+const morgan = require("morgan");
 
-app.set('port', port)
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.raw())
-app.use(express.urlencoded({ extended: false }))
+const routes = require("@app/routes/_index.routes");
+
+app.set("port", port);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
-})
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
-const mode = env.MODE === 'DEV' ? '/dev' : ''
+const mode = env.MODE === "DEV" ? "/dev" : "";
 
-app.use(mode + '/api/v1', routes)
+app.use(mode + "/api/v1", routes);
 /**
  * use folder page for rutes not enpoints
  */
-app.use("/public",express.static(path.join(__dirname, 'pages')))
-app.use('*', (req, res) => {
-  res.status(404).send({ status: 404, msg: "this endpoint doesn't exist", type: 'error', code: 404 })
-})
-
+app.use("/public", express.static(path.join(__dirname, "pages")));
+app.use("*", (req, res) => {
+    res.status(404).send({
+        status: 404,
+        msg: "this endpoint doesn't exist",
+        type: "error",
+        code: 404,
+    });
+});
 
 /**
  * app.listen
  * @description enpoint listen
  */
 
-module.exports = app
+module.exports = app;
