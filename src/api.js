cee3a55dc46bbc838cfe746d8565898439105327
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-15 18:51:35 -0500

    testing

--- src/api.js
+++ src/api.js
@@ -20,19 +20,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, DELETE"
-    );
-    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+//     );
+//     res.header(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, OPTIONS, PUT, DELETE"
+//     );
+//     res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+//     next();
+// });
 
 app.use(
     cors({
